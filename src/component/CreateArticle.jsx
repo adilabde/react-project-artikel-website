97c53aa420c77d7a787fe2b898b5f0f7ed105e0d
Adil Abde <adilabde1994@gmail.com>
2023-11-14 12:08:43 +0100

    create-form

--- src/component/CreateArticle.jsx
+++ src/component/CreateArticle.jsx
@@ -3,18 +3,41 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function CreateArticle() {
-  const [articleTitle, setArticleTitle] = useState("");
+  const [article, setArticle] = useState({
+    title: "",
+    date: "",
+  });
+
+
+
+
 
   const handleChangeArticle = (e) => {
-    setArticleTitle(e.target.value);
-    console.log(e.target.value);
+    setArticle({...article, [e.target.name]:e.target.value})
+
+    // switch (e.target.name) {
+    //   case "title":
+    //     setArticle({...article, title: e.target.value });
+    //     break;
+    //   case "date":
+    //     setArticle({...article, date: e.target.value });
+    //     break;
+
+    //   default:
+    //     // console.log(e.target.value);
+    //     break;
+    // }
   };
+  console.log(article.date,article.title);
   return (
     <div>
       <Navbar />
       <div className="create_article">
         <h1 className="create_article-title">Create Article</h1>
-        <InputData handleChangeArticle={handleChangeArticle} name={"Title"} />
+        <InputData handleChangeArticle={handleChangeArticle} name={"title"} />
+        <InputData handleChangeArticle={handleChangeArticle} name={"date"} />
+        <InputData handleChangeArticle={handleChangeArticle} name={"author"} />
+        <InputData handleChangeArticle={handleChangeArticle} name={"time to read"} />
       </div>
 
       <Footer />
@@ -22,13 +45,16 @@ export default function CreateArticle() {
   );
 }
 
-function InputData(props) {
+function InputData({ name, handleChangeArticle }) {
   return (
     <div className="input__content">
-      <label className="input__content-label">{props.name} : </label>
+      <label className="input__content-label">
+        {`${name.slice(0, 1).toUpperCase() + name.slice(1)}`} :{" "}
+      </label>
       <input
-        onChange={props.handleChangeArticle}
-        type="text"
+        name={name}
+        onChange={handleChangeArticle}
+        type={name==='date'?'date':'text'}
         className="input__content-input"
       />
     </div>
